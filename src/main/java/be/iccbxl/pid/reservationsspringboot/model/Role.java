2336ee93a82b96df8201a7118bb9f8c02aee6288
Mohamed <mamankour@outlook.be>
2025-04-04 09:25:32 +0200

    Relation ManyToMany between User & role

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Role.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Role.java
@@ -1,12 +1,16 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name="roles")
@@ -15,6 +19,15 @@ public class Role {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String role;
+	
+	@ManyToMany
+	@JoinTable(
+		  name = "user_role", 
+		  joinColumns = @JoinColumn(name = "role_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users = new ArrayList<>();
+	
+
 	
 	protected Role() {	}
 	
@@ -34,6 +47,28 @@ public class Role {
 	public void setRole(String role) {
 		this.role = role;
 	}
+    public List<User> getUsers() {
+		return users;
+	}
+
+	public Role addUser(User user) {
+		if(!this.users.contains(user)) {
+			this.users.add(user);
+			user.addRole(this);
+		}
+		
+		return this;
+	}
+	
+	public Role removeUser(User user) {
+		if(this.users.contains(user)) {
+			this.users.remove(user);
+			user.getRoles().remove(this);
+		}
+		
+		return this;
+	}
+
 
 	@Override
 	public String toString() {
--- src/main/java/be/iccbxl/pid/reservationsspringboot/repository/UserRepository.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/repository/UserRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import be.iccbxl.pid.reservationsspringboot.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	User findByLogin(String login);
 	List<User> findByLastname(String lastname);
-	 
+
 	User findById(long id);
+	User findByLogin(String login);
+	User findByEmail(String email);
 }
+
 
